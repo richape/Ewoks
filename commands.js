d91336b31356c282f9eb14219e13af29669c280b
rich-hape <richard.hape@devacademy.co.nz>
2023-08-25 15:04:59 +1200

    first commit

---
+++ commands.js
@@ -0,0 +1,23 @@
+import { getTodos, close } from './db.js'
+
+export async function list() {
+  try {
+    const todos = await getTodos()
+    printTodos(todos)
+  } catch (err) {
+    logError(err)
+  } finally {
+    close()
+  }
+}
+
+function printTodos(todos) {
+  todos.forEach((todo) => {
+    console.info(`${todo.id}: ${todo.task}`)
+  })
+}
+
+function logError(err) {
+  console.error('Uh oh!', err.message)
+}
+
---
+++ db.js
@@ -0,0 +1,14 @@
+import knexfile from './knexfile.js'
+import knex from 'knex'
+
+const db = knex(knexfile.development)
+
+export function getTodos() {
+  return db('todos').select()
+}
+
+// Your DB functions go here
+export function close() {
+  db.destroy()
+}
+// Hurry the F up
